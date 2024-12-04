caf10a2c672934f2c3b12a70066f7f9e5c8ac2bb
Yash Palhare <yash.palhare_24uit@sanjivanicoe.org.in>
2024-12-04 22:21:08 +0530

    added 2 files

---
+++ assignment7/program6.c
@@ -0,0 +1,15 @@
+//pattern
+#include<stdio.h>
+
+int main()
+{
+    for(int i=1; i<=4; i++)
+    {
+        for (int j=1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+        
+    }
+}
