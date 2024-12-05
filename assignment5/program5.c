a20db21626fc135efccf1e6adb23b36f3da139c7
Yash Palhare <yash.palhare_24uit@sanjivanicoe.org.in>
2024-12-05 22:43:31 +0530

    minor changes

--- assignment5/program5.c
+++ assignment5/program5.c
@@ -2,22 +2,25 @@
 #include<stdio.h>
 #include<math.h>
 
-int principal, rate, time, simple_i, compound_i, a;
+float principal, rate, time, simple_i, compound_i, a;
+int n;
 
 int main()
 {
     printf("Enter Principal amount:");
-    scanf("%d", &principal);
-    printf("Enter Rate of interest:");
-    scanf("%d", &rate);
+    scanf("%f", &principal);
+    printf("Enter Rate of interest in decimal form:");
+    scanf("%f", &rate);
     printf("Enter Time in years:");
-    scanf("%d", &time);
+    scanf("%f", &time);
+    printf("How many times in year will the interest be compounded?");
+    scanf("%d", &n);
 
     simple_i = principal*rate*time;
-    a = principal*pow((1+rate/1), time);
+    a = principal*pow((1+rate/n), n*time);
     compound_i = a-principal;
 
-    printf("Simple interest is: %d\n", simple_i);
-    printf("Compound interest is: %d\n", compound_i);
+    printf("Simple interest is: %.2f\n", simple_i);
+    printf("Compound interest is: %.2f\n", compound_i);
 }
 
---
+++ assignment6/program7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+void findRoots(float a, float b, float c) {
+    float discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {
+        float root1 = (-b + sqrt(discriminant)) / (2 * a);
+        float root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("The equation has two real and distinct roots: %.2f and %.2f\n", root1, root2);
+    }
+    else if (discriminant == 0)
+    {
+        float root = -b / (2 * a);
+        printf("The equation has one real root: %.2f\n", root);
+    }
+    else
+    {
+        float realPart = -b / (2 * a);
+        float imaginaryPart = sqrt(-discriminant) / (2 * a);
+        printf("The equation has two complex roots: %.2f + %.2fi and %.2f - %.2fi\n", realPart, imaginaryPart, realPart, imaginaryPart);
+    }
+}
+
+int main() {
+    float a, b, c;
+
+    printf("Enter coefficients a, b, and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0) {
+        printf("Invalid input! 'a' cannot be zero for a quadratic equation.\n");
+    } else {
+        findRoots(a, b, c);
+    }
+
+    return 0;
+}
